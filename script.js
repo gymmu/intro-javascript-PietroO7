78e040fac4b208c291125426627694116195a7b7
Pietro D'Ambrosio <pietro.d2007@gmail.com>
2023-10-23 12:12:31 +0200

    Aufgabe 5 + 6

--- script.js
+++ script.js
@@ -18,3 +18,19 @@ function incrementCounter() {
   num = num + 1
   numberElement.textContent = num
 }
+
+function decrementCounter() {
+  const numberElement = document.querySelector("#counter")
+  let num = parseInt(numberElement.textContent)
+  num = num - 1
+  numberElement.textContent = num
+}
+function resetCounter() {
+    const numberElement = document.querySelector("#counter")
+    let num = parseInt(numberElement.textContent)
+    num = 0
+    numberElement.textContent = num
+}
+function writeText() {
+  
+}
